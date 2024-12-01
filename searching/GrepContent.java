8ca6962f0e173473b60cea9313795a592fd5c4fb
Rajib Kumar <rajib2516@student.nstu.edu.bd>
2024-12-01 10:28:57 +0600

    2. smell: Dead Code, refactor: Delete the dead code

--- searching/GrepContent.java
+++ searching/GrepContent.java
@@ -32,27 +32,6 @@ public class GrepContent {
         // System.out.println("" + result);
         return result;
     }
-
-    /* public int grepLineNumber(String word, String file,int preLine) throws FileNotFoundException, IOException {
-        String str;
-        int linenumber = 0;
-        BufferedReader buf = new BufferedReader(new InputStreamReader(new DataInputStream(new FileInputStream(file))));
-      
-        String line;
-        int LineNumber = 0;
-        while ((line = buf.readLine()) != null) {
-            LineNumber++;
-            //     System.out.println("pop");
-            if (line.contains(word)) {
-                //System.out.println("po" + LineNumber);
-                linenumber = LineNumber;
-                break;
-               // System.out.println("" + linenumber);
-            }
-        }
-        //   System.out.println(""+LineNumber);
-        return linenumber;
-    }*/
     public int getLineNumber(String word, String file, int preLine) throws FileNotFoundException, IOException {
     //    System.out.println(word);
         int lineNumber = 0;
